ca1a4e21598a31c48e8f247c75f7bdf51ff846f8
MhmdRyhn <rayhan.cse12@gmail.com>
2020-06-25 02:51:56 +0600

    Add routing rules and route tables

--- terraform/input_value.tfvars
+++ terraform/input_value.tfvars
@@ -1,3 +1,4 @@
+# ---------- Change these values as your requirement -------------
 access_key        = "aws-access-key"
 secret_access_key = "aws-secret-access-key"
 region            = "your-preffered-region"
---
+++ terraform/routing.tf
@@ -0,0 +1,35 @@
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.main_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+  tags = {
+    Name = "${local.prefix}-public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_routing" {
+  for_each       = local.azs
+  route_table_id = aws_route_table.public_route_table.id
+  subnet_id      = aws_subnet.public_subnet[each.key].id
+}
+
+
+resource "aws_route_table" "private_route_table" {
+  for_each = local.azs
+  vpc_id   = aws_vpc.main_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat_gateway[each.key].id
+  }
+  tags = {
+    Name = "${local.prefix}-private-route-table-${each.key}"
+  }
+}
+
+resource "aws_route_table_association" "private_subnet_routing" {
+  for_each       = local.azs
+  route_table_id = aws_route_table.private_route_table[each.key].id
+  subnet_id      = aws_subnet.private_subnet[each.key].id
+}
