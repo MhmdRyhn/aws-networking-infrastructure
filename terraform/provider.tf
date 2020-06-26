5346519125799895c4eb80cb60741437a64a5c73
MhmdRyhn <rayhan.cse12@gmail.com>
2020-06-26 22:40:54 +0600

    Comment terraform aws provider to reuse this module

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -7,8 +7,8 @@
 //  }
 //}
 
-provider "aws" {
-  region     = var.region
-  access_key = var.access_key
-  secret_key = var.secret_access_key
-}
+//provider "aws" {
+//  region     = var.region
+//  access_key = var.access_key
+//  secret_key = var.secret_access_key
+//}
